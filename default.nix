0f6140a7a811ea09773fe71fda98512a4b262ff0
Denbeigh Stevens <denbeigh.stevens@discordapp.com>
2022-06-03 08:12:50 -0700

    darwin: do not use vim-go due to bug

--- default.nix
+++ default.nix
@@ -1,6 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs }:
 
 let
+  inherit (pkgs.stdenv) hostPlatform;
+
+  # NOTE: vim-go currently broken on darwin due to https://github.com/NixOS/nixpkgs/issues/168984
+  # Attempted using overlay solutions: https://github.com/sagikazarmark/go-bin-flake
+  # but this does not seem to overwrite the version of golangci-lint used by vimPlugins.vim-go.
+
+  extraPlugins = (
+  if hostPlatform.isDarwin
+  then []
+  else [ pkgs.vimPlugins.vim-go ]
+  );
+
   plugins = with pkgs.vimPlugins; [
     LanguageClient-neovim
     nvim-yarp
@@ -24,11 +36,10 @@ let
     vim-puppet
     arcanist-vim
     vim-protobuf
-    vim-go
     vim-jsx-typescript
     vim-javascript
     gruvbox
-  ];
+  ] ++ extraPlugins;
 in
   pkgs.neovim.override {
     configure = {
