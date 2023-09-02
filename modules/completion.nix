0f6c0640c8d7830e7584e3abfda7937a4a46647a
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-09-02 14:01:44 -0500

    completion: use luasnip for snippets

--- modules/completion.nix
+++ modules/completion.nix
@@ -4,56 +4,61 @@ let
   inherit (lib) mkDefault;
 in
 {
-  config = {
-    plugins.nvim-cmp = {
-      enable = mkDefault true;
-      completion.keywordLength = 0;
-      sources = [
-        { name = "buffer"; }
-        { name = "path"; }
-      ];
+  config.plugins = {
+    luasnip.enable = true;
+    nvim-cmp =
+      {
+        enable = mkDefault true;
+        preselect = "None";
+        completion.keywordLength = 0;
+        snippet.expand = "luasnip";
+        sources = [
+          { name = "luasnip"; }
+          { name = "buffer"; }
+          { name = "path"; }
+        ];
 
-      # https://old.reddit.com/r/neovim/comments/vr6kwr/help_to_make_ctrlnp_navigation_in_command_lsp/iet92xt/
-      mapping = {
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<C-n>" = ''
-          cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                vim.api.nvim_feedkeys(t("<Down>"), "n", true)
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                fallback()
-              end
-            end,
-          })
-        '';
+        # https://old.reddit.com/r/neovim/comments/vr6kwr/help_to_make_ctrlnp_navigation_in_command_lsp/iet92xt/
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<C-n>" = ''
+            cmp.mapping({
+              c = function()
+                if cmp.visible() then
+                  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  vim.api.nvim_feedkeys(t("<Down>"), "n", true)
+                end
+              end,
+              i = function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  fallback()
+                end
+              end,
+            })
+          '';
 
-        "<C-p>" = ''
-          cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                vim.api.nvim_feedkeys(t("<Up>"), "n", true)
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                fallback()
-              end
-            end,
-          })
-        '';
+          "<C-p>" = ''
+            cmp.mapping({
+              c = function()
+                if cmp.visible() then
+                  cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  vim.api.nvim_feedkeys(t("<Up>"), "n", true)
+                end
+              end,
+              i = function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                else
+                  fallback()
+                end
+              end,
+            })
+          '';
+        };
       };
-    };
   };
 }
